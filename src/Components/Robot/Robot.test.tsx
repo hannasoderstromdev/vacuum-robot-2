c52775f2e5cffb15812b96eb87da6bc90704d7f4
Hanna Söderström <hanna.soderstrom@netigate.se>
2020-04-03 11:02:31 +0200

    test: render and not render

--- src/Components/Robot/Robot.test.tsx
+++ src/Components/Robot/Robot.test.tsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import { render } from '@testing-library/react'
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import { render } from '@testing-library/react';
 
-import Robot from './Robot'
+import Robot from './Robot';
+
+const mockStore = configureMockStore();
 
 describe('Robot', () => {
-  it('renders', () => {
-    const { getByTestId } = render(<Robot id="x0y0" />)
+  it('renders when coordinates match', () => {
+    const store = mockStore({
+      robot: {
+        x: 0,
+        y: 0,
+      },
+    });
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <Robot id="x0y0" />
+      </Provider>,
+    );
 
-    expect(getByTestId('robot')).toBeDefined()
-  })
-})
+    expect(getByTestId('robot')).toBeDefined();
+  });
+  it("doesn't render when coordinates don't match", () => {
+    const store = mockStore({
+      robot: {
+        x: 0,
+        y: 1,
+      },
+    });
+    const { queryByTestId } = render(
+      <Provider store={store}>
+        <Robot id="x0y0" />
+      </Provider>,
+    );
+
+    expect(queryByTestId('robot')).toEqual(null);
+  });
+});
